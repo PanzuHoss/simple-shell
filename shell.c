529f152188d94329655e320889da7507a24396c8
arnac <arnavgudadhe1@gmail.com>
2026-05-12 19:50:21 +0530

    shell project 1.0

--- shell.c
+++ shell.c
@@ -2,50 +2,51 @@
 #include<stdlib.h>
 #include<string.h>
 #include<sys/wait.h>
+#include <unistd.h>
 
-#define TOK_DELIM" \t\r\n"
-#define RED" \033[0;31m"
-#define RESET" \e[0m"
+#define TOK_DELIM " \t\r\n"
+#define RED " \033[0;31m"
+#define RESET " \e[0m"
 #define TK_BUFF_SIZE 64
 
-int exit(char *);
-int loop(char **);
-char **split();
-char *read();
-int execute(char **);
+int s_exit(char **);
+void s_loop();
+char **split_line(char *);
+char *read_line();
+int s_execute(char **);
 
 void main(){
-    loop();
+    s_loop();
 }
 
-int exit(char *){
+int s_exit(char **args){
     return 0;
 }
 
-int loop(char **){
-    int a=0;
+void s_loop(){
+    int a=1;
     char *line, **args;
     do{
         printf("> ");
-        line = read();
-        args = split(line);
+        line = read_line();
+        args = split_line(line);
+        a=s_execute(args);
         free(line);
         free(args);
     }while(a);
 }
 
-char read(char *){
+char *read_line(){
     
-    int buffersize=1024, position = 0;
-    int * buffer = malloc(buffersize * sizeof(char));
-    int c;
+    int buffersize=1024, position = 0, c;
+    char * buffer = malloc(buffersize * sizeof(char));
 
     if(!buffer){
         fprintf(stderr,"%s Allocation Error! %s\n",RED,RESET);
         exit(EXIT_FAILURE);
     }
 
-    else{
+    while(1){
         c = getchar();
         if(c==EOF || c =='\n'){
             buffer[position]='\0';
@@ -68,17 +69,17 @@ char read(char *){
     }
 }
 
-char **split(char *line){
-    int buffersize= TK_BUFF_SIZE, position = 0;+ 
+char **split_line(char *line){
+    int buffersize= TK_BUFF_SIZE, position = 0; 
     char **tokens = malloc(buffersize*sizeof(char *));
     char *token;
 
-    if(!token){
+    if(!tokens){
         fprintf(stderr,"%s Error in memory allocation (**split)! %s \n",RED, RESET);
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(NULL,TOK_DELIM);
+    token = strtok(line,TOK_DELIM);
     while(token != NULL){
         tokens[position]=token;
         position++;
@@ -95,4 +96,30 @@ char **split(char *line){
     }
     tokens[position]=NULL;
     return tokens;
+}
+
+int s_execute(char **args){
+    pid_t cpid;
+    int status;
+   
+    if(args[0]==NULL) return 1;
+
+    if(strcmp(args[0],"exit")==0){
+        return s_exit(args);
+    }
+    cpid=fork();
+    
+    if(cpid==0){
+        if(execvp(args[0],args)<0){
+            printf("Invalid Command : %s\n",args[0]);
+            exit(EXIT_FAILURE);
+        };
+    }
+        
+    else if(cpid<0)
+       printf(RED "ERROR IN FORKING!" RESET "\n");
+
+    else
+        waitpid(cpid,&status,WUNTRACED);
+    return 1;
 }
